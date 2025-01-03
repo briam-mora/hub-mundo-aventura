c58506f61df38cd0a46124f8d35d4d632be1b398
EmanuelMenaCENFOTEC <emenaa@ucenfotec.ac.cr>
2025-01-03 07:54:43 -0600

    mejora en el reactive

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import Logo from './Logo';
 import Button from './Button';
 
-const Header: React.FC = () => {
+type HeaderProps = {
+    showMuteButton?: boolean;
+    showBackButton?: boolean;
+    url?: string;
+};
+
+const Header: React.FC<HeaderProps> = ({ showMuteButton = false, showBackButton = false, url = 'URL(CAMBIAR)' }) => {
     return (
         <div className="header">
-            <Logo src='src/assets/images/Logo Mundo Aventura.png' alt='Logo Museo de los Niños' />
-            <Button id='mute-btn' ></Button>
+            <Logo src='src/assets/images/Logo Mundo Aventura.png' alt='Logo de Experiencia Mundo Aventura' />
+            <div className='io-btn-container'>
+                {showBackButton && <Button id='back-btn' onClick={() => window.open(url, "_self")} />}
+                {showMuteButton && <Button id='mute-btn' />}
+            </div>
         </div>
     );
 };
--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,7 +1,7 @@
 import React, {useEffect} from 'react';
 import Header from './Header';
 import Button from './Button';
-import Logo from './Logo';
+import Footer from './Footer';
 
 const MainLayout: React.FC = () => {
     function setVh() {
@@ -20,16 +20,16 @@ const MainLayout: React.FC = () => {
 
     return (
         <div className="main-layout">
-            <Header />
+            <Header showBackButton={false} showMuteButton={false}/>
+            <div className='info-container'>
+                <p>Diviértete completando las 3 actividades</p>
+            </div>
             <div className="button-container">
                 <Button id="button-one" label="" onClick={ () => window.open("https://festivaldelaluz.8thwall.app/stickers-animados/", "_self") } />
                 <Button id="button-two" label="" onClick={ () => window.open("https://festivaldelaluz.8thwall.app/filtro-abeja/", "_self") } />
                 <Button id="button-three" label="" onClick={ () => window.open("https://festivaldelaluz.8thwall.app/ciudad-sostenible/", "_self") } />
             </div>
-            <div className="footer">
-                <Logo src='src/assets/images/museo_de_los_niños.svg' alt = 'Logo del Museo de los Niños' />
-                <Logo src='src/assets/images/cenfotec.svg' alt='Logo Universidad Cenfotec' />
-            </div>
+            <Footer/>
         </div>
     );
 };
